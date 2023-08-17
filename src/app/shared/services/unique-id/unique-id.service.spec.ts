a26d116850361de01557767a2c8871c8847dd60f
Lucas <lucasdec88@gmail.com>
2023-08-17 11:27:33 +0100

    withContext() to know the element inside an array

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -9,7 +9,7 @@ describe(UniqueIdService.name, () => {
 
   //before each test, this method will be called
   beforeEach(() => {
-    service = new UniqueIdService();//creates the instance
+    service = new UniqueIdService(); //creates the instance
   });
 
   it('should be created', () => {
@@ -19,7 +19,8 @@ describe(UniqueIdService.name, () => {
   //The base strucute: blablabla should blablabla when blablabla
   //UniqueIdService.prototype.generateUniqueIdWithPrefix.name is to use the
   //method name after someone refactor the name
-  it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+  it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name}
+    should generate id when called with prefix`, () => {
     const id = service.generatedUniqueIdWithPrefix('app');
 
     expect(id.startsWith('app-')).toBeTrue();
@@ -27,29 +28,32 @@ describe(UniqueIdService.name, () => {
 
   it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name}
     should not generate duplicate ID when called multiple times`, () => {
-      const ids = new Set();//it's an array that not accept duplicate items
+    const ids = new Set(); //it's an array that not accept duplicate items
 
-      for (let i = 0; i < 50; i++) {
-        ids.add(service.generatedUniqueIdWithPrefix('app'));
-      }
-      //since it's an array without duplicate IDs, it should be has 50 elements
-      expect(ids.size).toBe(50);
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generatedUniqueIdWithPrefix('app'));
+    }
+    //since it's an array without duplicate IDs, it should be has 50 elements
+    expect(ids.size).toBe(50);
   });
 
   it(`#${UniqueIdService.prototype.getNumberOfGeneratedIds.name}
     should return the number of generatedIds when called.`, () => {
-      const id1 = service.generatedUniqueIdWithPrefix('app');
-      const id2 = service.generatedUniqueIdWithPrefix('app');
+    const id1 = service.generatedUniqueIdWithPrefix('app');
+    const id2 = service.generatedUniqueIdWithPrefix('app');
 
-      expect(service.getNumberOfGeneratedIds()).toBe(2);
+    expect(service.getNumberOfGeneratedIds()).toBe(2);
   });
 
   it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name}
     should throw an Exception when empty`, () => {
-      const emptyValues = [null, undefined, '', '0', '1'];
-      emptyValues.forEach( element => {
-        //when testing an Exception, we need to write '() =>' inside the the expect
-        expect( () => service.generatedUniqueIdWithPrefix(element)).toThrow();
-      });
+    const emptyValues = [null, undefined, '', '0', '1'];
+    emptyValues.forEach((element) => {
+      //when testing an Exception, we need to write '() =>' inside the the expect
+      expect(() => service.generatedUniqueIdWithPrefix(element))
+        ////withContext() is used to know which element inside the array is giving the error
+        .withContext(`Empty value: ${element}`)
+        .toThrow();
+    });
   });
 });
