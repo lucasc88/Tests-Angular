dae5c4d71ff53fdc29b67b0f080bb1ec3eb2361d
Lucas <lucasdec88@gmail.com>
2023-08-21 14:52:24 +0100

    detectChanges() was explained

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -4,43 +4,41 @@ import { LikeWidgetComponent } from './like-widget.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 describe(LikeWidgetComponent.name, () => {
-
   let component: LikeWidgetComponent;
   let fixture: ComponentFixture<LikeWidgetComponent>;
 
   //This structure is mandatory.
   //compileComponents() method will be called asynchronously.
   //Each test must be run independently.
+  //fixture.detectChanges() must be outside the beforeEach() to be able to correctly test the
+  // @Input() component properties
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LikeWidgetComponent ],
-      imports: [ FontAwesomeModule ] //sometimes we need to import some external modules for you componment
-    })
-    .compileComponents();
+      declarations: [LikeWidgetComponent],
+      imports: [FontAwesomeModule], //sometimes we need to import some external modules for you componment
+    }).compileComponents();
 
     //fixture it's a wrapper that has the component instance and other utility methods.
     fixture = TestBed.createComponent(LikeWidgetComponent);
     component = fixture.componentInstance;
-
-    //detectChanges() triggers the angular life cicle (onInit(), onChanges()...)
-    fixture.detectChanges();
   });
 
   it('should create component', () => {
+    //detectChanges() triggers the angular life cicle (onInit(), onChanges()...)
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 
   it('Should generate ID when id input property is missing', () => {
+    fixture.detectChanges();
     expect(component.id).toBeTruthy();
   });
 
-
-  it('Should NOT generate ID when id input property is present' , () => {
+  it('Should NOT generate ID when id input property is present', () => {
     const myId = 'ID-123';
     component.id = myId;
     //onInit() will check that exist a value and it will not be changed
+    fixture.detectChanges();
     expect(component.id).toBe(myId);
   });
-
-
 });
