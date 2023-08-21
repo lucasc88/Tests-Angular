ac5472fb6a74ead6d3fdf8c8a54b3ee85c7b0e11
Lucas <lucasdec88@gmail.com>
2023-08-21 14:05:08 +0100

    How to test intialy a component

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,25 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LikeWidgetComponent } from './like-widget.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-describe('LikeWidgetComponent', () => {
+describe(LikeWidgetComponent.name, () => {
+
   let component: LikeWidgetComponent;
   let fixture: ComponentFixture<LikeWidgetComponent>;
 
+  //This structure is mandatory.
+  //compileComponents() method will be called asynchronously.
+  //Each test must be run independently.
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LikeWidgetComponent ]
+      declarations: [ LikeWidgetComponent ],
+      imports: [ FontAwesomeModule ] //sometimes we need to import some external modules for you componment
     })
     .compileComponents();
-  });
 
-  beforeEach(() => {
+    //fixture it's a wrapper that has the component instance and other utility methods.
     fixture = TestBed.createComponent(LikeWidgetComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
   });
+
 });
